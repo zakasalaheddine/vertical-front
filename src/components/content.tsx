414d87d010e6881e5b8a32313f63c09e99c73a37
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2022-11-19 16:24:18 +0100

    feat: add responsive

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,7 +4,7 @@ export default function Content({ data }: any) {
   return (
     <ContentSection
       dangerouslySetInnerHTML={{ __html: data }}
-      className="container max-w-4xl mx-auto my-6"
+      className="container max-w-4xl mx-auto my-6 px-3 md:px-0"
     ></ContentSection>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import { useDirectusFileUrl } from '../hooks/useDirectusFileUrl'
 export default function Header({ headline, thumbnail, excerpt }: any) {
   const thumbnailSrc = useDirectusFileUrl(thumbnail)
   return (
-    <header className="h-[80vh] w-screen ">
-      <div className="m-6 h-full overflow-hidden rounded-3xl relative drop-shadow-2xl">
+    <header className="md:h-[80vh] w-screen mb-6">
+      <div className="m-6 md:h-full overflow-hidden rounded-3xl relative drop-shadow-2xl">
         {thumbnailSrc ? (
           <Image
             src={thumbnailSrc}
@@ -16,14 +16,20 @@ export default function Header({ headline, thumbnail, excerpt }: any) {
             priority
           />
         ) : null}
-        <div className="absolute bottom-0 h-full w-full bg-gradient-to-b from-transparent to-black/70 flex flex-col px-10 justify-end pb-2">
-          <h1 className="font-bold text-6xl text-white">{headline}</h1>
+        <div className="absolute bottom-0 h-full w-full bg-gradient-to-b from-transparent to-black/70 flex flex-col px-3 md:px-10 justify-end pb-2">
+          <h1 className="font-bold text-2xl md:text-6xl text-white">
+            {headline}
+          </h1>
           <div
             dangerouslySetInnerHTML={{ __html: excerpt }}
-            className="py-6 text-2xl text-white"
+            className="py-6 hidden md:block md:text-2xl text-white"
           ></div>
         </div>
       </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: excerpt }}
+        className="pb-6 block md:hidden md:text-2xl text-black px-3"
+      ></div>
     </header>
   )
 }
